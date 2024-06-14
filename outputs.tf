76a94d2ca57634fba0e6468e54d4755c50ffc866
Julien WITTOUCK <julien.wittouck@gmail.com>
2024-06-14 12:12:31 +0200

    📝 : add output description

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
 output "cluster_kubeconfig" {
-  value = nonsensitive(scaleway_k8s_cluster.cluster.kubeconfig[0].config_file)
+  description = "the contents of generated KUBECONFIG file"
+  value       = nonsensitive(scaleway_k8s_cluster.cluster.kubeconfig[0].config_file)
 }
